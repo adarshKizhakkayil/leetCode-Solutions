9c0fcf675228afd5d56f132cc950fd544a3308ba
adarshKizhakkayil <146309769+adarshKizhakkayil@users.noreply.github.com>
2025-09-30 10:47:57 +0530

    Time: 32 ms (39.79%) | Memory: 55.1 MB (57.04%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,15 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let mp = new Map()
-    
-    for (let i = 0; i < nums.length; i++) {
-        let diff = target - nums[i]
-        
-        if (mp.has(diff)) {
-            return [i, mp.get(diff)]
+ for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (target === nums[i] + nums[j]) {
+                return [i, j];
+            }
         }
-        
-        mp.set(nums[i], i)
     }
 };
